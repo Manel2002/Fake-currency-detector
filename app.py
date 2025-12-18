b4e2edf12b1e80e08767c05dc0cc45525ff44b21
Manel Hamlaoui <88409564+Manel2002@users.noreply.github.com>
2025-12-18 04:04:46 +0100

    Change joblib import alias and model loading
    Refactor joblib import and update model loading.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-import joblib
+import joblib as load 
 import numpy as np
 
+import sys
+st.write(sys.executable)
+
 # loading the model 
-model = joblib.load("logistic_currency_model.pkl")
+model = load("logistic_currency_model.pkl")
 
 st.title("💵 Fake Currency Detection System")
 st.write("Enter the geometric features of the banknote:")
